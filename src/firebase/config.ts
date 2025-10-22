7258a6ac4f2338d9178edf22a9358296bfbb336d
Philjoseph Orlina <philjoseph547@gmail.com>
2025-10-22 02:02:30 +0000

    there's an issue with the apikey we used. because it is publicly leaked

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-7875916541-41745",
-  "appId": "1:1093682758531:web:778c9ca4c9e98f3775acb6",
-  "apiKey": "AIzaSyCr4Q8Kk_47jStr8kbvBKkt6RfrMWWllS8",
-  "authDomain": "studio-7875916541-41745.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1093682758531"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
